a998235799b82e5521358029c62925d9478a0dd3
Ayush9040 <68855277+Ayush9040@users.noreply.github.com>
2022-02-01 11:33:43 +0545

    Add files via upload

---
+++ Escape Sequence.c
@@ -0,0 +1,86 @@
+/* \n use to go in seprate line */
+
+/*
+main()
+{
+    getch();
+    printf("Hello\nStudents");
+    getch();
+}
+
+*/
+
+
+/* tab \t use cases */
+/* It print 8 spaces */
+
+/*
+main()
+{
+    getch();
+    printf("\tHello\tStud\tents");
+    getch();
+}
+*/
+
+/* back space \b use cases */
+
+/*
+main()
+{
+    getch();
+    printf("Hello \bStudents");
+    getch();
+}
+*/
+
+/* back space \r use cases */
+
+/*
+
+main()
+{
+    getch();
+    printf("Surya\r Div");
+    printf("Surya\r");
+    getch();
+}
+
+*/
+
+/* back space \\ use cases */
+
+/*
+
+main(){
+
+printf("\\Surya\\Dev");
+getch();
+}
+
+*/
+
+/* back space \" use cases */
+
+/*
+
+main(){
+
+printf("\"Surya\"");
+getch();
+}
+
+*/
+
+
+/* back space \' use cases */
+
+/*
+
+main(){
+
+printf("\'Surya\'");
+getch();
+}
+
+*/
