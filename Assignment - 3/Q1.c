a5740bf8db75f4f1ae34b2ad6a5acffa717b9ba4
Ayush9040 <68855277+Ayush9040@users.noreply.github.com>
2022-02-09 15:08:57 +0545

    Create Q1.c

---
+++ Assignment - 3/Q1.c
@@ -0,0 +1,14 @@
+main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a<=0)
+    {
+        printf("A is non-positive.");
+    }
+    else
+    {
+        printf("A is positive.");
+    }
+    getch();
+}
