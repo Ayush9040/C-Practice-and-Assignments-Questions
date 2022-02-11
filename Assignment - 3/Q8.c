21a372cf91e9b8b5fb3b1207291b1f65fc7cb280
Ayush9040 <68855277+Ayush9040@users.noreply.github.com>
2022-02-11 15:11:52 +0545

    Create Q8.c

---
+++ Assignment - 3/Q8.c
@@ -0,0 +1,22 @@
+main()
+{
+    int a = 15, b = 8;
+
+
+
+    if(a>b)
+    {
+        printf("A is greater");
+    }
+    else
+    {
+        if(a == b)
+    {
+        printf("Both are Equal.");
+    }
+    else
+    {
+        printf("B is greater");
+    }
+    }
+}
