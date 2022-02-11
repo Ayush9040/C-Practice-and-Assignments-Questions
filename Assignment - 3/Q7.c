49c104f22d2d52cb497833e68bed1fc5cb696a5c
Ayush9040 <68855277+Ayush9040@users.noreply.github.com>
2022-02-11 15:04:37 +0545

    Create Q7.c

---
+++ Assignment - 3/Q7.c
@@ -0,0 +1,20 @@
+main()
+{
+    int a = -10;
+    if(a>0)
+    {
+        printf("Positive");
+    }
+    else
+    {
+        if(a<0)
+        {
+            printf("Negative");
+        }
+        else
+        {
+            printf("Zero");
+
+        }
+    }
+}
